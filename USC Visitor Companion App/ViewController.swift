2aadae7dec9252ee9b6343ba574d246887ccf69c
Christian Villa <iamchristianv@gmail.com>
2016-09-24 13:15:06 -0700

    Initial Commit

---
+++ USC Visitor Companion App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  USC Visitor Companion App
+//
+//  Created by Christian Villa on 9/24/16.
+//  Copyright © 2016 University of Southern California. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
