beb46fa3474fb0979a3179c064c1c03c448a2d71
Christian Villa <iamchristianv@gmail.com>
2016-09-28 14:22:49 -0700

    Add location model

---
+++ USC Visitor Companion App/Location.swift
@@ -0,0 +1,18 @@
+//
+//  Location.swift
+//  USC Visitor Companion App
+//
+//  Created by Christian Villa on 9/27/16.
+//  Copyright © 2016 University of Southern California. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class Location: NSObject {
+    
+    // MARK: Constructors
+    init(object: PFObject) {
+        
+    }
+}
