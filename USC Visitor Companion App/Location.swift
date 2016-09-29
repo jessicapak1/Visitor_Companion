ee6f6d694bde7cb11d6a4c397558e4a9badab2e6
Christian Villa <iamchristianv@gmail.com>
2016-09-28 17:57:49 -0700

    Update getters and setters for location model

--- USC Visitor Companion App/Location.swift
+++ USC Visitor Companion App/Location.swift
@@ -11,8 +11,73 @@ import Parse
 
 class Location: NSObject {
     
-    // MARK: Constructors
-    init(object: PFObject) {
-        
+    // MARK: Location Properties
+    var name: String? {
+        get { return self.value(forKey: "name") }
+        set { self.setValue(newValue, forKey: "name") }
     }
+    
+    var code: String? {
+        get { return self.value(forKey: "code") }
+        set { self.setValue(newValue, forKey: "code") }
+    }
+    
+    var details: String? {
+        get { return self.value(forKey: "details") }
+        set { self.setValue(newValue, forKey: "details") }
+    }
+    
+    var coordinate: CLLocation? {
+        get { return self.value(forKey: "coordinate") }
+        set { self.setValue(newValue, forKey: "coordinate") }
+    }
+    
+    var interests: [String]? {
+        get { return self.value(forKey: "interests") }
+        set { self.setValue(newValue, forKey: "interests") }
+    }
+    
+    
+    // MARK: Constructor
+    init(object: PFObject) {
+        super.init()
+        self.name = object.value(forKey: "name") as? String
+        self.code = object.value(forKey: "code") as? String
+        self.details = object.value(forKey: "details") as? String
+        self.coordinate = object.value(forKey: "coordinate") as? CLLocation
+        self.interests = object.value(forKey: "interests") as? [String]
+    }
+    
+    
+    // MARK: Location Methods
+    
+
+    // MARK: Private Methods
+    private func value(forKey key: String) -> String? {
+        if let currentUser = PFUser.current() {
+            return currentUser.value(forKey: key) as? String
+        }
+        return nil
+    }
+    
+    private func value(forKey key: String) -> CLLocation? {
+        if let currentUser = PFUser.current() {
+            return currentUser.value(forKey: key) as? CLLocation
+        }
+        return nil
+    }
+    
+    private func value(forKey key: String) -> [String]? {
+        if let currentUser = PFUser.current() {
+            return currentUser.value(forKey: key) as? [String]
+        }
+        return nil
+    }
+    
+    private func setValue(value: Any?, forKey key: String) {
+        if let currentUser = PFUser.current() {
+            currentUser.setValue(value, forKey: key)
+        }
+    }
+    
 }
