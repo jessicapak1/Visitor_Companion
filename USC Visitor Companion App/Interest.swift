bbff02d78d097143f6a36e83a16b665094e06ae8
Christian Villa <iamchristianv@gmail.com>
2016-09-28 18:26:07 -0700

    Add interest model

---
+++ USC Visitor Companion App/Interest.swift
@@ -0,0 +1,17 @@
+//
+//  Interest.swift
+//  USC Visitor Companion App
+//
+//  Created by Christian Villa on 9/28/16.
+//  Copyright © 2016 University of Southern California. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class Interest: NSObject {
+
+    // MARK: Interest Properties
+    var name: String?
+    
+}
