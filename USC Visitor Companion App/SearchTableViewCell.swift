9f4fb8cf7413c67dec2cf1f8b2780b7b7183dc03
Christian Villa <iamchristianv@gmail.com>
2016-10-04 18:49:01 -0700

    Add search table view cell

---
+++ USC Visitor Companion App/SearchTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SearchTableViewCell.swift
+//  USC Visitor Companion App
+//
+//  Created by Christian Villa on 10/4/16.
+//  Copyright © 2016 University of Southern California. All rights reserved.
+//
+
+import UIKit
+
+class SearchTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
