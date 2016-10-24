884d34abdb1c0debf617bce259973963a0fccf1a
edgarlug <edgarlug@usc.edu>
2016-10-24 13:25:45 -0700

    checkin/location screen much much better!

---
+++ USC Visitor Companion App/LocationTableViewController.swift
@@ -0,0 +1,84 @@
+//
+//  LocationTableViewController.swift
+//  USC Visitor Companion App
+//
+//  Created by Edgar Lugo on 10/24/16.
+//  Copyright © 2016 University of Southern California. All rights reserved.
+//
+
+import UIKit
+
+class LocationTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    //make navbar transparent
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        self.navigationController?.navigationBar.isTranslucent = true
+        /*
+        self.navigationController?.navigationBar.layer.shadowColor = UIColor.black.cgColor
+        self.navigationController?.navigationBar.layer.shadowOffset = CGSize(width: 0.0, height: 3)
+        self.navigationController?.navigationBar.layer.shadowOpacity = 0.25
+        self.navigationController?.navigationBar.layer.masksToBounds = false
+        self.navigationController?.navigationBar.layer.shouldRasterize = true
+         */
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        imageView.image = UIImage(named: "tommy trojan")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.row == 0 {
+            return 200.0
+        } else if indexPath.row == 1 {
+            return 160.0
+        }
+        return 44.0
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.row == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "blankCell", for: indexPath)
+            return cell
+        } else if indexPath.row == 1 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "descriptionCell", for: indexPath)
+            return cell
+        } else if indexPath.row == 2 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "mediaCell", for: indexPath)
+            return cell
+        } else if indexPath.row == 3 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "instagramCell", for: indexPath)
+            return cell
+        } else {
+            let cell = UITableViewCell()
+            return cell
+        }
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
