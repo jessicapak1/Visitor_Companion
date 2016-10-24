887b06c878bc501580f15e721c040e3e005eaff8
Jessica Pak <pakjessi@usc.edu>
2016-10-23 21:28:39 -0700

    nothing

--- USC Visitor Companion App/AdminTableViewController.swift
+++ USC Visitor Companion App/AdminTableViewController.swift
@@ -21,6 +21,7 @@ class AdminTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
