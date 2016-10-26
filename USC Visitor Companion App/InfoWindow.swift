f06ee2d1254f0fb6cc12dc8712f818f8bb2060ae
Sarah Dervartanian <dervarta@usc.edu>
2016-10-25 22:06:25 -0700

    set up custom info window, crashes when click on marker though

---
+++ USC Visitor Companion App/InfoWindow.swift
@@ -0,0 +1,14 @@
+//
+//  InfoWindow.swift
+//  USC Visitor Companion App
+//
+//  Created by Sarah Dervartanian on 10/25/16.
+//  Copyright © 2016 University of Southern California. All rights reserved.
+//
+
+import UIKit
+
+class InfoWindow: UIView {
+    @IBOutlet weak var locationNameLabel: UILabel!
+    
+}
--- USC Visitor Companion App/MapViewController.swift
+++ USC Visitor Companion App/MapViewController.swift
@@ -344,5 +344,11 @@ class MapViewController: UIViewController, GMSMapViewDelegate, UIViewControllerT
         }
     }
     
+    func mapView(_ mapView: GMSMapView, markerInfoWindow marker: GMSMarker) -> UIView? {
+        let infoWindow = Bundle.main.loadNibNamed("InfoWindow", owner: self, options: nil)?.first! as! InfoWindow
+        infoWindow.locationNameLabel.text = "Hope this works"
+        return infoWindow
+    }
+    
 
 }
