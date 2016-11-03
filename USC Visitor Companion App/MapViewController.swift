4cb61cabaccea9dd1f7a5e4d1a0eb662052b67ab
Sarah Dervartanian <dervarta@usc.edu>
2016-11-02 22:16:16 -0700

    will create marker when near certain location

--- USC Visitor Companion App/MapViewController.swift
+++ USC Visitor Companion App/MapViewController.swift
@@ -128,26 +128,40 @@ class MapViewController: UIViewController, GMSMapViewDelegate, UIViewControllerT
 
     func locationManager(_: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let userLocation:CLLocation = locations[0]
-//        print("locations = \(userLocation.coordinate.latitude) \(userLocation.coordinate.longitude)")
-    }
-    
-    func nearby() {
-        //OR USE PARSE???? CHRISTIAN HALP
+        //print("locations = \(userLocation.coordinate.latitude) \(userLocation.coordinate.longitude)")
+        // nearby locations?
+        print("in location manager didUpdateLocations func")
         var distance : CLLocationDistance?
-        let maxDistance = CLLocationDistance(10)
-        for location in LocationData.shared.locations {
-            distance = locationManager.location?.distance(from: location.location!)
-            if distance! < maxDistance {
+        let maxDist = CLLocationDistance(10)
+        /*
+        for location in LocationData.shared.locations{
+            distance = userLocation.distance(from: location.location!)
+            if distance! < maxDist {
                 let marker = GMSMarker()
                 marker.map = self.mapView
                 marker.icon = GMSMarker.markerImage(with: UIColor.red)
+                marker.position = (location.location?.coordinate)!
             }
         }
+        */
+        // temp for nearby location testing
+        
+        let house : CLLocation = CLLocation(latitude: 34.024771, longitude: -118.281451)
+        distance = userLocation.distance(from: house)
+        if distance! < maxDist {
+            let marker = GMSMarker()
+            marker.map = self.mapView
+            marker.title = "ADX House"
+            marker.snippet = "This is where sarah lives"
+            marker.position = CLLocationCoordinate2D(latitude: 34.024771, longitude: -118.281451)
+            marker.icon = GMSMarker.markerImage(with: UIColor.black)
+        }
+        
         
     }
     
-    
     func showMarkers() {
+        
         //create custom marker icons
         let foodImage = UIImage(named: "food")!.withRenderingMode(.alwaysTemplate)
         let foodView = UIImageView(image: foodImage)
